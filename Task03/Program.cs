dbef5d9bc655cfa38b95df5ee1bc25d21c61f690
Dmitry <Logan6338@mail.ru>
2022-08-12 19:04:28 +0300

    Задача 3

---
+++ Task03/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void find();
+{
+  
+if (6 <= num <= 7)
+{
+    Console.WriteLine("Да");
+}
+else
+
+    if (0 < num < 6)
+    {
+        Console.WriteLine("Нет");
+    }
+}
+find();
