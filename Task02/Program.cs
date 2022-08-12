cbb0dab8a0fce0094a5ec60c5d65013685ca3988
Dmitry <Logan6338@mail.ru>
2022-08-12 19:03:19 +0300

    Задача 2

---
+++ Task02/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Напишите число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+void find()
+{
+    Console.WriteLine();
+    Console.Write((numberA / 100) % 10);
+}
+
+find();
