f2c28cfb55966b050be1018479410d79f2b5823c
Dmitry <Logan6338@mail.ru>
2022-08-16 18:12:34 +0300

    Задача 3

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,10 +1,24 @@
-﻿Console.WriteLine("Напишите число");
-int numberA = Convert.ToInt32(Console.ReadLine());
+﻿//Console.WriteLine("Напишите число");
+//int numberA = Convert.ToInt32(Console.ReadLine());
 
-void find()
+//void find()
+//{
+//    Console.WriteLine();
+//    Console.Write((numberA / 100) % 10);
+//}
+
+///find();
+
+Console.WriteLine("Напишите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int find(int n)
 {
-    Console.WriteLine();
-    Console.Write((numberA / 100) % 10);
-}
+    int[] array = new int[n];
 
-find();
+    for (int i = 2; i <= n; i++)
+    {
+        array[i - 1] = i * i;
+    }
+    return n;
+}
